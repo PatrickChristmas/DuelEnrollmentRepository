7c4cef2ab1fc0582b3f51c0dbcfd3fb307e81f33
Patrick.Christmas26 <Patrick.Christmas26@2025-313-4.bi.local>
2024-11-04 10:37:59 -0500

    Second Commit

---
+++ GithubAssignment1/src/GithubProjectThingy.java
@@ -0,0 +1,12 @@
+// Patrick Christmas
+
+public class GithubProjectThingy {
+
+	public GithubProjectThingy() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) { 
+	System.out.println("Github Project Assignment");
+}
+
+}
